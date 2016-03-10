3947b4c0b1fe67b60d7fff9733e4fdf6d6ed7234
Ian Alonzo <ianoalonzo@gmail.com>
2016-03-10 21:52:25 +0800

    Finished Hamming Distance (but not integrated as a function.

--- bioinformaticslibrary.js
+++ bioinformaticslibrary.js
@@ -1,2 +1,37 @@
 //Programming Assignment 002
 //Simple Bioinformatics Library
+
+var string1 = "AT";
+var string2 = "AG";
+var hammingDistance = 0;
+
+//Checks if string1 and string2's length are greater than zero
+if ((string1.length > 0) && (string2.length > 0)) {
+
+	//Check if string1 and string2 have equal length
+	if (string1.length == string2.length) {
+
+		//Traverses through each element of the string
+		for (var i = 0; i < string1.length; i++) {
+			
+			//If elements of each string are not equal, add 1 to hamming distance
+			if(string1[i] != string2[i]) {
+				hammingDistance++;
+			}
+
+		}
+
+		//Prints the Hamming Distance
+		console.log("Hamming Distance: " + hammingDistance);
+
+	} else {
+
+		console.log("ERROR: Strings are not equal.");
+
+	}
+
+} else {
+
+	console.log("ERROR: One or both of the strings has a length equal or less than 0.");
+
+}
