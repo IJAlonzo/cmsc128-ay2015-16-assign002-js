88de2fad7661a9c4fb89936424173c4e770849d9
Ian Alonzo <ianoalonzo@gmail.com>
2016-03-11 16:20:37 +0800

    Done with getHammingDistance Function.

--- bioinformaticslibrary.js
+++ bioinformaticslibrary.js
@@ -1,37 +1,40 @@
 //Programming Assignment 002
 //Simple Bioinformatics Library
 
-var string1 = "AT";
-var string2 = "AG";
-var hammingDistance = 0;
+function getHammingDistance(string1, string2) 
+{
+	var hammingDistance = 0;
 
-//Checks if string1 and string2's length are greater than zero
-if ((string1.length > 0) && (string2.length > 0)) {
+	//Checks if string1 and string2's length are greater than zero
+	if ((string1.length > 0) && (string2.length > 0)) {
 
-	//Check if string1 and string2 have equal length
-	if (string1.length == string2.length) {
+		//Check if string1 and string2 have equal length
+		if (string1.length == string2.length) {
 
-		//Traverses through each element of the string
-		for (var i = 0; i < string1.length; i++) {
-			
-			//If elements of each string are not equal, add 1 to hamming distance
-			if(string1[i] != string2[i]) {
-				hammingDistance++;
+			//Traverses through each element of the string
+			for (var i = 0; i < string1.length; i++) {
+				
+				//If elements of each string are not equal, add 1 to hamming distance
+				if(string1[i] != string2[i]) {
+					hammingDistance++;
+				}
+
 			}
+
+			//Prints the Hamming Distance
+			console.log("Hamming Distance: " + hammingDistance);
+
+		} else {
+
+			console.log("ERROR: Strings are not equal.");
 
 		}
 
-		//Prints the Hamming Distance
-		console.log("Hamming Distance: " + hammingDistance);
-
 	} else {
 
-		console.log("ERROR: Strings are not equal.");
+		console.log("ERROR: One or both of the strings has a length equal or less than 0.");
 
 	}
 
-} else {
-
-	console.log("ERROR: One or both of the strings has a length equal or less than 0.");
-
+	return hammingDistance;
 }
